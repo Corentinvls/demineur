6b466c93b027933ebec33fb7d3d06f267d8e5ce9
Marion <meurantmarion@gmail.com>
2020-01-15 13:44:29 +0100

    Chrono starts but doesn't stop

---
+++ script.js
@@ -0,0 +1,49 @@
+var setTm = 0;
+var tmStart = 0;
+var tmNow = 0;
+var tmInterv = 0;
+var tTime = [];
+var nTime = 0;
+function affTime(tm) {
+      var vMin = tm.getMinutes();
+      var vSec = tm.getSeconds();
+      if (vMin < 10) {
+            vMin = "0" + vMin;
+      }
+      if (vSec < 10) {
+            vSec = "0" + vSec;
+      }
+
+      document.getElementById("chronotime").innerHTML = vMin + ":" + vSec;
+}
+function fChrono() {
+      tmNow = new Date();
+      Interv = tmNow - tmStart;
+      tmInterv = new Date(Interv);
+      affTime(tmInterv);
+}
+function newGame() {
+      fStop();
+      if (tmInterv == 0) {
+            tmStart = new Date();
+      } else {
+            tmNow = new Date();
+            Pause = tmNow - tmInterv;
+            tmStart = new Date(Pause);
+      }
+      setTm = setInterval(fChrono, 50);
+}
+
+function fStop() {
+      clearInterval(setTm);
+      tTime[nTime] = tmInterv;
+}
+
+function fReset() {
+      fStop();
+      tmStart = 0;
+      tmInterv = 0;
+      tTime = [];
+      nTime = 0;
+      document.getElementById("chronotime").innerHTML = "00:00";
+}
