c25aed8b63e4692cadf5f9c74acd79b71f6412e1
vallois <corentin.vallois@edu.itescia.fr>
2020-01-16 13:59:46 +0100

    left click

--- assets/chrono.js
+++ assets/chrono.js
@@ -4,9 +4,12 @@ function newGame() {
     fStart();
     grid = new Array();
     bombsIndex = new Array();
+    revealed = new Array();
+    temp = new Array();
     generategrid(10);
-    generateBombs(20);
+    generateBombs(10);
     generateHtml();
+
 }
 
 //genere une grille carré d'une taille demander en paramètre
@@ -20,6 +23,7 @@ function generategrid(size) {
         }
     }
 }
+
 //rempli la grille en fonction du nombre de bombe en paramètre
 function generateBombs(nbBomb) {
     //boucle pour avoir le nombre de bombe demander
@@ -34,28 +38,29 @@ function generateBombs(nbBomb) {
         //mise en place des bombes
         setTileValue(x, y, 9);
         // tableau de coordonée de bombe
-        bombsIndex.push([x,y])
+        bombsIndex.push([x, y])
         //incrémente les cases adjacantes
-        updateTiles(x,y)
+        updateTiles(x, y)
     }
 }
+
 //incrémente les case adjacante au bombe
-function updateTiles(x,y){
+function updateTiles(x, y) {
 //defilement de haut en bas
-    for (j = x-1; j <= x+1; j++) {
+    for (j = x - 1; j <= x + 1; j++) {
         //gestion hors map
-        if (j==-1 || j == (grid.length))
+        if (j == -1 || j == (grid.length))
             continue;
         //defilement de gauche a droite
-        for (k = y-1; k <= y+1; k++) {
+        for (k = y - 1; k <= y + 1; k++) {
             //gestion hors map
-            if (k==-1 || k == (grid.length))
+            if (k == -1 || k == (grid.length))
                 continue;
-                //incrémente la case si ce n'est pas une bombe
-                if (grid[j][k] != 9)
-                    grid[j][k]++;
-            }
+            //incrémente la case si ce n'est pas une bombe
+            if (grid[j][k] != 9)
+                grid[j][k]++;
         }
+    }
 
 }
 
@@ -69,101 +74,149 @@ function getTileValue(indexX, indexY) {
 }
 
 function generateHtml() {
-    for(o=0;o<grid.length;o++){
-        for(p=0;p<grid.length;p++){
+    for (o = 0; o < grid.length; o++) {
+        for (p = 0; p < grid.length; p++) {
             var img = document.createElement('img');
             img.setAttribute("src", "assets/images/normal.png");
-            img.setAttribute("id","'"+o.toString()+":"+p.toString()+"'")
+            img.setAttribute("id", "'" + o.toString() + ":" + p.toString() + "'")
             var att = document.createAttribute("onclick");
             att.value = "selectedTile(" + o.toString() + "," + p.toString() + ")";
             img.setAttributeNode(att);
             document.getElementById("game").appendChild(img);
-        };
+        }
+        ;
     }
 }
+
 function reset() {
     document.getElementById("game").innerHTML = '';
 }
-function selectedTile(x,y) {
-            reveal(x,y);
-            if (grid[x][y] == 9)
-                lose();
-            if(grid[x][y] == 0)
-                revealAdj(x,y);
-}
-function revealAdj(x,y) {
-    reveal(x,y);
-    for (j = x-1; j <= x+1; j++) {
-        if (j==-1 || j == (grid.length))
-            continue;
-        for (k = y-1; k <= y+1; k++) {
-            if (k==-1 || k == (grid.length))
-                continue;
-            reveal(j,k);
-        }
+
+function selectedTile(x, y) {
+
+
+
+    if (grid[x][y] == -1) {
+        return;
     }
-}
-function reveal(x,y) {
-if(grid[x][y]==0)
-    document.getElementById("'"+x.toString()+':'+y.toString()+"'").src= "assets/images/empty.png";
-if(grid[x][y]==9)
-    document.getElementById("'"+x.toString()+':'+y.toString()+"'").src= "assets/images/bomb.png";
-if(grid[x][y]!=0 && grid[x][y]!=9)
-    document.getElementById("'"+x.toString()+':'+y.toString()+"'").src= "assets/images/"+grid[x][y]+".png";
+
+    if (grid[x][y] == 9) {
+        reveal(x, y);
+        lose();
+        return;
+    }
+
+
+
+
+
+    if (grid[x][y] == 0) {
+        reveal(x,y)
+        for (j = x - 1; j <= x + 1; j++) {
+            if (j == -1 || j == (grid.length))
+                continue;
+            for (k = y - 1; k <= y + 1; k++) {
+                if (k == -1 || k == (grid.length))
+                    continue;
+                if (grid[j][k] == -1)
+                    continue;
+                if (grid[j][k] > 0)
+                    reveal(j,k)
+                if (grid[j][k] == 0 && !myinclude(temp, [j, k])) {
+                    temp.push([j, k]);
+
+                }
+
+            }
+        }
+    while(temp.length!=0){autoReveal();}
+    }
+
+    if(grid[x][y] > -1){reveal(x, y);}
+
 
 }
-function lose(){
-    console.log("vous vous etes pris une bombe")
-}
+
+    function autoReveal() {
+            tempx = temp[0][0]
+            tempy = temp [0][1]
+            temp.shift()
+            selectedTile(tempx, tempy)
+    }
+
+    function myinclude(tab, element) {
+        bool = false
+        for (a = 0; a < tab.length; a++) {
+            if (tab[a] == element) {
+                bool = true
+                return bool
+            }
+        }
+        return bool
+    }
 
 
+    function reveal(x, y) {
 
+        document.getElementById("'" + x.toString() + ':' + y.toString() + "'").onclick = "";
+        if (grid[x][y] == 0)
+            document.getElementById("'" + x.toString() + ':' + y.toString() + "'").src = "assets/images/empty.png";
+        if (grid[x][y] == 9)
+            document.getElementById("'" + x.toString() + ':' + y.toString() + "'").src = "assets/images/bomb.png";
+        if (grid[x][y] != 0 && grid[x][y] != 9)
+            document.getElementById("'" + x.toString() + ':' + y.toString() + "'").src = "assets/images/" + grid[x][y] + ".png";
+        setTileValue(x, y, -1)
+    }
+
+    function lose() {
+        console.log("vous vous etes pris une bombe")
+    }
 
 
 //section du chrono
-var setTm = 0;
-var start = 0;
-var now = 0;
-var diff = 0;
-var tabTime = [];
-var nTime = 0;
+    var setTm = 0;
+    var start = 0;
+    var now = 0;
+    var diff = 0;
+    var tabTime = [];
+    var nTime = 0;
 
-function affTime(tm) {
-    var minutes = tm.getMinutes();
-    var secondes = tm.getSeconds();
+    function affTime(tm) {
+        var minutes = tm.getMinutes();
+        var secondes = tm.getSeconds();
 
-    if (minutes < 10) {
-        minutes = "0" + minutes;
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (secondes < 10) {
+            secondes = "0" + secondes;
+        }
+        document.getElementById("chronotime").innerHTML = minutes + ":" + secondes;
     }
-    if (secondes < 10) {
-        secondes = "0" + secondes;
-    }
-    document.getElementById("chronotime").innerHTML = minutes + ":" + secondes;
-}
 
-function fChrono() {
-    now = new Date();
-    Interv = now - start;
-    diff = new Date(Interv);
-    affTime(diff);
-}
-
-function fStart() {
-    fReset();
-    if (diff == 0) {
-        start = new Date();
-    } else {
+    function fChrono() {
         now = new Date();
-        Pause = now - diff;
-        start = new Date(Pause);
+        Interv = now - start;
+        diff = new Date(Interv);
+        affTime(diff);
     }
-    setTm = setInterval(fChrono, 10);
-}
 
-function fReset() { //on efface tout
-    start = 0;
-    diff = 0;
-    tabTime = [];
-    nTime = 0;
-    document.getElementById("chronotime").innerHTML = "00:00";
-}
+    function fStart() {
+        fReset();
+        if (diff == 0) {
+            start = new Date();
+        } else {
+            now = new Date();
+            Pause = now - diff;
+            start = new Date(Pause);
+        }
+        setTm = setInterval(fChrono, 10);
+    }
+
+    function fReset() { //on efface tout
+        start = 0;
+        diff = 0;
+        tabTime = [];
+        nTime = 0;
+        document.getElementById("chronotime").innerHTML = "00:00";
+    }
