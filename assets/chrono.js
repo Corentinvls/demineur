0779985d31cee2e1b93f10fcf867d4e21441a98e
DogSobre <juanfernandoherrada13@gmail.com>
2020-01-16 10:51:29 +0100

    jsp toujours pas

--- assets/chrono.js
+++ assets/chrono.js
@@ -1,11 +1,16 @@
-
-function newGame() {
-    fStart()
+function difficulties(){
+    var easy = {lines: 9, rows: 9, bombs: 10};
+}
+    
+function newGame(difficulty) {
+    fStart();
+    this.settings;
     grid = new Array();
     bombsIndex = new Array();
-    generategrid(4);
-    generateBombs(10);
-    console.log(bombsIndex)
+    this.settings = this.difficulties(difficulty);
+    this.generategrid();
+    
+
 }
 
 
@@ -28,7 +33,28 @@ function generateBombs(nbBomb) {
         }
         setTileValue(x, y, 9);
         bombsIndex.push([x,y])
+        //incrémente les cases adjacantes
+        updateTiles(x,y)
     }
+}
+//incrémente les case adjacante au bombe
+function updateTiles(x,y){
+//defilement de haut en bas
+    for (j = x-1; j <= x+1; j++) {
+        //gestion hors map
+        if (j==-1 || j == (grid.length))
+            continue;
+        //defilement de gauche a droite
+        for (k = y-1; k <= y+1; k++) {
+            //gestion hors map
+            if (k==-1 || k == (grid.length))
+                continue;
+                //incrémente la case si ce n'est pas une bombe
+                if (grid[j][k] != 9)
+                    grid[j][k]++;
+            }
+        }
+
 }
 
 function setTileValue(rowIndex, columnIndex, value) {
@@ -42,6 +68,29 @@ function getTileValue(indexX, indexY) {
     return grid[indexX][indexY];
 }
 
+function generateHtml() {
+   // document.getElementById('game').style.display=grid;
+   // document.getElementById('game').style.justifyContent=center;
+  //  document.getElementById('game').style.alignItems=center;
+   // document.getElementById('game').style.gridTemplateColumn=repeat(grid.length, grid.length);
+    for(o=0;o<grid.length;o++){
+        for(p=0;p<grid.length;p++){
+            var img = document.createElement('img');
+            img.setAttribute("src", "assets/images/normal.png");
+            img.setAttribute("id","'"+o.toString()+":"+p.toString()+"'")
+            var att = document.createAttribute("onclick");
+            att.value = "selectedTile(" + o.toString() + "," + p.toString() + ")";
+            img.setAttributeNode(att);
+            document.getElementById("game").appendChild(img);
+        };
+    }
+}
+
+
+
+
+
+//section du chrono
 var setTm = 0;
 var start = 0;
 var now = 0;
